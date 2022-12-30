c8583f30d69ea5f6c089df820b7a334e97bc46a0
Martin Mwangi <mwangimartin1904@gmail.com>
2022-12-31 00:59:50 +0300

    Load persisted posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AddPostForm from './components/AddPostForm'
 import Header from './components/Header'
 import PostsList from './components/PostsList'
+import { loadPostsAction } from './redux/actions/postActions'
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const posts = JSON.parse(localStorage.getItem('posts') || "[]")
+
+    console.log(posts);
+    if (posts?.length > 0) {
+      dispatch(loadPostsAction(posts))
+    }
+  }, [dispatch])
+  
   return (
     <div className='bg-[#f0f0f0] min-h-screen'>
       <Header />
